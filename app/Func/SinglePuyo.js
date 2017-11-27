34da742fba32b2d83e610b2f20683bf2bab83d88
Hou In Choi <hichoi17@outlook.com>
2017-11-27 11:51:54 -0500

    fix(eslint) fix single eslint

--- app/Func/SinglePuyo.js
+++ app/Func/SinglePuyo.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-const colors = ['#f4eee1', '#ebcfc4', '#e8e6d9', '#999999', '#e9ccb1']
-=======
-let colors = ['#ffe3e3', '#fffdda', '#e0ffdc', '#ccfff6', '#deddff']
->>>>>>> bdfa434b80fe7077196cd26711fb3b7e74ab43fd
+const colors = ['#ffe3e3', '#fffdda', '#e0ffdc', '#ccfff6', '#deddff']
 function randomColor() {
   return colors[Math.floor(Math.random() * colors.length)]
 }
