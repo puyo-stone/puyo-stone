9d1d642954192ea39fff7cabf622f75ab24fdde7
bluufish <simonmchan92@gmail.com>
2017-11-26 20:23:02 -0500

    f/add css to start screen

---
+++ app/components/StartMenu.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router'
+
+class StartMenu extends Component {
+
+    render() {
+        return (
+            <div className="start container">
+                <div className="start overlay single"> 
+                <h1>SinglePlayer!</h1>
+                </div>
+                <div className="start overlay multiplayer">
+                <h1>MultiPlayer!</h1>
+                </div>
+                <div className="start overlay tutorial">
+                <h1>HighScore!</h1>
+                </div>
+                <div className="start overlay highscore">
+                <h1>Tutorial!</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default StartMenu
