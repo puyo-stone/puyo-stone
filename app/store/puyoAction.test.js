861242a181222cb1ddb833da72dbf0ef21d8b815
bluufish <simonmchan92@gmail.com>
2017-12-06 16:02:11 -0500

    chore: delete empty files

--- app/store/puyoAction.test.js
+++
@@ -1,8 +0,0 @@
-import chai, {expect} from 'chai'
-import puyoAction from './puyoAction'
-
-/* global describe it beforeEach */
-
-describe('Current Puyo Reducer', () => {
-
-})
--- app/store/puyoColors.test.js
+++
@@ -1,8 +0,0 @@
-import chai, {expect} from 'chai'
-import puyoColors from './puyoColors.js'
-
-/* global describe it beforeEach */
-
-describe('Puyo Color Reducer', () => {
-
-})
