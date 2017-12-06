27386aed027affeceb8acafe2647bd8bf5da9838
Yvette Zhang <yvettez2016@gmail.com>
2017-12-06 15:38:24 -0500

    merg: merged with master, took off comment

--- app/store/puyoAction.test.js
+++ app/store/puyoAction.test.js
@@ -1,4 +1,4 @@
-import chai, {expect} from 'chai'
+import chai, {expect, beforeEach, afterEach, describe, it} from 'chai'
 import puyoAction, { leftMove, dropMove, rotateA } from './puyoAction.js'
 import DoublePuyo from '../Func/DoublePuyo';
 import configureMockStore from 'redux-mock-store';
@@ -6,7 +6,6 @@ import thunkMiddleware from 'redux-thunk';
 
 const middlewares = [thunkMiddleware]
 const mockStore = configureMockStore(middlewares)
-/* global describe it beforeEach afterEach */
 
 describe('Puyo Actions', () => {
   let init, store
